77f4936d527952b34f7f3955a5754d21d34e195d
Saleik <sarrazinkevin@free.fr>
2021-09-20 16:08:35 +0200

    update rules api

--- backend/server.ts
+++ backend/server.ts
@@ -50,7 +50,10 @@ app.use((req, res, next) => {
 
 /** Rules of our API */
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.header(
+		'Access-Control-Allow-Origin',
+		'https://candidate.sarrazin-kevin.fr'
+	);
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
